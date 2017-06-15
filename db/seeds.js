19ecbc3b0663499374104b2a07620ac9008173c4
Angela Maugey <a.maugey@gmail.com>
2017-06-15 10:54:03 +0100

    heroku push

--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-const { dbURI } = require('./config/environment');
+const { dbURI } = require('../config/environment');
 
 
 mongoose.connect(dbURI);
