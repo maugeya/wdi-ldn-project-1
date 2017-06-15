bbed683200d239190ce8234705b67d518e84ebb2
Angela Maugey <a.maugey@gmail.com>
2017-06-15 11:00:37 +0100

    heroku push

--- config/oauth.js
+++ config/oauth.js
@@ -4,7 +4,7 @@ module.exports = {
     client_id: process.env.FACEBOOK_APP_ID,
     client_secret: process.env.FACEBOOK_APP_SECRET,
     accessTokenURL: 'https://graph.facebook.com/v2.9/oauth/access_token',
-    redirect_uri: 'http://localhost:8000/oauth/facebook',
+    redirect_uri: process.env.NODE_ENV === 'production' ? 'https://ancient-dusk-97846.herokuapp.com/oauth/facebook' : 'http://localhost:8000/oauth/facebook',
     scope: 'email',
     getLoginURL() {
       return `${this.login_url}?client_id=${this.client_id}&redirect_uri=${this.redirect_uri}&scope=${this.scope}`;
