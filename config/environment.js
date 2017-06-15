23f2036f4f219500e14b0f011feded2a856e3c79
Angela Maugey <a.maugey@gmail.com>
2017-06-15 10:48:36 +0100

    trying to push to heroku

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ const port = process.env.PORT || 3000;
 
 const env = process.env.NODE_ENV || 'development';
 
-const dbURI = process.env.MONGOD_URI || 'mongodb://localhost/wdi-ldn-project-1';
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/wdi-ldn-project-1';
 
 const sessionSecret = process.env.SESSION_SECRET || 'ssh it\'s a secret';
 
